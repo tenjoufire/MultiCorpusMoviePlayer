3b84359b7c7094079ffe109d3f0e6f05198f73bc
Kaisei Tsujimoto <puyopuyo.fv@gmail.com>
2017-08-01 18:33:08 +0900

    sliderの実装

--- MultiCorpusMoviePlayer/MainWindow.xaml.cs
+++ MultiCorpusMoviePlayer/MainWindow.xaml.cs
@@ -27,6 +27,9 @@ namespace MultiCorpusMoviePlayer
 
         string fileName = "";
 
+        //再生中かどうか
+        bool isPlaying = false;
+
         public MainWindow()
         {
             InitializeComponent();
@@ -44,11 +47,51 @@ namespace MultiCorpusMoviePlayer
             storyboard.Children.Add(mediaTimeline);
             storyboard.Begin(this, true);
 
+            //コントロールの変更
+            SliderTime.IsEnabled = true;
+            Play.IsEnabled = true;
+            Stop.IsEnabled = true;
+
+            isPlaying = true;
+
+        }
+
+        private void StopMovie()
+        {
+            //ストーリーボードの停止
+            storyboard.Stop(this);
+            storyboard.Children.Clear();
+            storyboard = null;
+
+            //コントロールの変更
+            SliderTime.Value = 0.0;
+            SliderTime.IsEnabled = false;
+            Stop.IsEnabled = false;
+
+            isPlaying = false;
+
         }
 
         private void Play_Click(object sender, RoutedEventArgs e)
         {
-
+            //何も再生されていないときは新しく再生を始める
+            if(storyboard == null)
+            {
+                PlayMovie(); 
+            }
+            else //すでに再生されている場合は，一時停止か再び再生か
+            {
+                if (isPlaying)
+                {
+                    storyboard.Pause(this);
+                }
+                else
+                {
+                    storyboard.Resume(this);
+                }
+                Play.Content = isPlaying ? "Pause" : "Play";
+                isPlaying = !isPlaying;
+            }
         }
 
         private void OpenFile_Click(object sender, RoutedEventArgs e)
@@ -64,12 +107,54 @@ namespace MultiCorpusMoviePlayer
             //開いたファイルを表示
             FileName.Text = fileName.Split("\\"[0]).LastOrDefault();
 
+            if(storyboard != null)
+            {
+                StopMovie();
+            }
+
             PlayMovie();
         }
 
         private void mediaTimeline_CurrentTimeInvalidated(object sender, EventArgs e)
         {
+            if(storyboard != null)
+            {
+                SliderTime.Value = MoviePlayer.Clock.CurrentTime.Value.TotalMilliseconds;
 
+                if(MoviePlayer.NaturalDuration.HasTimeSpan && MoviePlayer.Clock.CurrentTime.Value.TotalMilliseconds == MoviePlayer.NaturalDuration.TimeSpan.TotalMilliseconds)
+                {
+                    StopMovie();
+                }
+            }
+        }
+
+        private void Stop_Click(object sender, RoutedEventArgs e)
+        {
+            StopMovie();
+        }
+
+        private void MoviePlayer_MediaOpened(object sender, RoutedEventArgs e)
+        {
+            SliderTime.Maximum = MoviePlayer.NaturalDuration.TimeSpan.TotalMilliseconds;
+        }
+
+        private void SliderTime_DragStarted(object sender, System.Windows.Controls.Primitives.DragStartedEventArgs e)
+        {
+            if (isPlaying)
+            {
+                storyboard.Pause(this);
+            }
+        }
+
+        private void SliderTime_DragCompleted(object sender, System.Windows.Controls.Primitives.DragCompletedEventArgs e)
+        {
+            //動画をシークする
+            storyboard.Seek(this, new TimeSpan((long)Math.Floor(SliderTime.Value * TimeSpan.TicksPerMillisecond)), TimeSeekOrigin.BeginTime);
+
+            if (isPlaying)
+            {
+                storyboard.Resume(this);
+            }
         }
     }
 }
